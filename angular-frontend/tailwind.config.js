f2936249f8e5f8b7010e90cbf5f4f310d1cf9adc
Jesús Gómez Beltrán <jgomezbeltran88@gmail.com>
2025-04-16 19:23:54 +0200

    Se añade paleta de colores, tipos y tamaños de letras y se hacen algunas configuraciones previas de estilos. Closes #4

--- angular-frontend/tailwind.config.js
+++ angular-frontend/tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
       "./src/**/*.{html,ts}",
     ],
     theme: {
-      extend: {},
+      extend: {
+        fontFamily: {
+          montserrat: ["Montserrat", "sans-serif"],
+          logo: ['Square Peg', 'cursive'],
+        },
+        colors: {
+          logo: "#D1D5DB",
+          letra: "#3B3B3B",
+          bordes: "#3a6d6e",
+        },
+        fontSize: {
+          xs2: "0.65rem", // Extra pequeño
+          mediana: "1.125rem", // entre base y lg
+          logo_letra: "2.75rem", // títulos o logo
+        },
+      },
     },
     plugins: [],
   }
