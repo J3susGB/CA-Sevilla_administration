b5598eef2f4faa721c28309d65c84b12fd9e74e6
Jesús Gómez Beltrán <jgomezbeltran88@gmail.com>
2025-04-18 20:26:40 +0200

    Se oculta el botón de Logout en las vistas de login y reporte. Closes #12

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './shared/header/header.component';  // <-- importa el header
+import { Router, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';  // importa el header
 
 @Component({
   selector: 'app-root',
   standalone: true,    
-  imports: [RouterOutlet, HeaderComponent],  // <-- añadir header aquí también
+  imports: [RouterOutlet, HeaderComponent],  // añadir header aquí también
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-frontend';
+  currentRoute: string = '';
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      this.currentRoute = this.router.url;
+    });
+  }
+
+  shouldShowLogoutButton(): boolean {
+    return !(this.currentRoute.includes('login') || this.currentRoute.includes('reporte'));
+  }
 }
